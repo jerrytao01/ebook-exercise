a3af4fc894da7805726f553713e58899f2b71cf2
jerrytao01 <841869544@qq.com>
2019-06-24 22:32:07 +0800

    初始化

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,15 +1,8 @@
 <template>
   <div class="about">
-    <h1 v-text="msg"></h1>
   </div>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      msg: 'This is test'
-    }
-  }
-}
+  export default {}
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span class="icon-bookmark"></span>
+    <span class="icon-clock"></span>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'home'
   }
-}
 </script>
+
+<style scoped>
+  .icon-bookmark {
+    color: red;
+    font-size: 100px;
+  }
+</style>
